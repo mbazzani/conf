e56be9d214d1aa8ab3014bc65777f76a6a7e9396
Marco Bazzani <mbazzani@ucsd.edu>
2023-09-20 08:09:00 -0700

    Add qtile config

---
+++ qtile/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+picom --vsync &
+setxkbmap -option caps:swapescape
+nitrogen --set-zoom-fill --random /usr/share/backgrounds/
+
+#Output name changes on reboot, this way at least one will be activated
+xrandr --output HDMI-1-0 --mode 2560x1440 &
+xrandr --output HDMI-1-1 --mode 2560x1440 &
+xrandr --output HDMI-1-2 --mode 2560x1440 &
+
+picom &
+
